06bbd03799f804f1e5c58f5d5d66f07746affa96
Bowen Wang <iwangbowen@gmail.com>
2025-02-17 12:45:59 +0800

    更新 src/index.ts 文件：移除样式依赖的引入，确保组件类型声明的正确性。

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import { App } from 'vue'
 import ExpressionEditor from './components/ExpressionEditor.vue'
 
-// 确保样式依赖被正确加载，注意引入顺序
-import './components/styles/variables.scss'
-import './components/styles/base.scss'
-import './components/styles/buttons.scss'
-import './components/styles/input.scss'
-import './components/styles/layout.scss'
-import './components/styles/theme.scss'
-
 // 添加组件类型声明
 declare module 'vue' {
   export interface GlobalComponents {
