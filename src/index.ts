f61a343cb77fbfd18c66dd7280ff3be01c166311
Bowen Wang <iwangbowen@gmail.com>
2025-02-17 11:30:30 +0800

    更新版本号：将版本从 1.0.1 升级到 1.0.2 优化组件导出方式：将 install 方法直接绑定到 ExpressionEditor 组件上

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { App } from 'vue'
 import ExpressionEditor from './components/ExpressionEditor.vue'
 
-export { ExpressionEditor }
-
-export default {
-  install: (app: App) => {
-    app.component('ExpressionEditor', ExpressionEditor)
-  }
+const install = (app: App) => {
+  app.component('ExpressionEditor', ExpressionEditor)
 }
+
+// 直接导出组件
+ExpressionEditor.install = install
+
+export { ExpressionEditor }
+export default ExpressionEditor
